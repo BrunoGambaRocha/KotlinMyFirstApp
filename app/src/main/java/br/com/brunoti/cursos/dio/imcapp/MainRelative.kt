fa19b345ecba5a6b7f843add653ceb456f05bd96
BrunoGambaRocha <bruno.roch@gmail.com>
2021-07-28 23:20:03 -0300

    RelativeLayout exercise with StartActivity changed

---
+++ app/src/main/java/br/com/brunoti/cursos/dio/imcapp/MainRelative.kt
@@ -0,0 +1,11 @@
+package br.com.brunoti.cursos.dio.imcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainRelative : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_relative)
+    }
+}
